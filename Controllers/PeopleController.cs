fcab3d6b4476191b189f93ecfc8106ea9244b77a
Isaiah Schmidt <isiahdschmidt@outlook.com>
2024-02-04 21:11:35 -0500

    styling done

--- Controllers/PeopleController.cs
+++ Controllers/PeopleController.cs
@@ -1,12 +1,24 @@
+using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
+using SwapiMVC.Models;
 
 namespace SwapiMVC.Controllers
 {
     public class PeopleController : Controller
     {
-        public IActionResult Index()
+        private readonly HttpClient _httpClient;
+        public PeopleController(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClient = httpClientFactory.CreateClient("swapi");
+        }
+        public async Task<IActionResult> Index(string page)
+        {
+            string route = $"people?page={page ?? "1"}";
+            HttpResponseMessage response = await _httpClient.GetAsync(route);
+            var responseString = await response.Content.ReadAsStringAsync();
+            var people = JsonSerializer.Deserialize<ResultsViewModel<PeopleViewModel>>(responseString);
+
+            return View(people);
         }
     }
 }
